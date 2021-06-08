e42d317aea86b40d4be122c60e66008d824e8758
Yan Zhang <248482914@qq.com>
2021-06-08 16:25:46 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="yanlp",
+  version="0.0.1",
+  author="skull.ai.research",
+  author_email="skull.ai.research@gmail.com",
+  description="yannlp",
+  long_description=long_description,
+  long_description_content_type="README.md",
+  url="https://github.com/skull-ai-research/yanlp",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
